11126b3ee6116907fa923aee0f823c3582059627
BrayanMen <brayanjmr880@gmail.com>
2023-07-31 00:28:35 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://BrayanMen.github.io/Todo-List/'
+  base: 'https://brayanmen.github.io/TodoList/'
 })
